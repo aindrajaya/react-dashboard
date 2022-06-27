d1d17e5b088d42e4c0d83963ba0e5250c5b77128
Arista Indrajaya <arista.indrajaya@gmail.com>
2022-06-27 23:12:21 +0700

    add app.js file

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+// import './App.css'
+
+const App = () => {
+  return (
+    // <h1 className="underline text-3xl">Berubah</h1>
+    <div>Without tailwind</div>
+  )
+}
+
+export default App
