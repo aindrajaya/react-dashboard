a79da93db5b74d03cc9e7b155144858891c4aec8
Arista Indrajaya <arista.indrajaya@gmail.com>
2022-06-29 05:22:00 +0700

    update setting button
    add final styling on it

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,14 @@ const App = () => {
         <div className='flex relative dark:bg-main-darg-bg'>
           <div className='fixed right-4 bottom-4' style={{zIndex: '1000'}}>
             <TooltipComponent content="Settings" position="Top">
-              <button>
+              <button
+                type='button'
+                className='text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white'
+                style={{
+                  background: 'blue', 
+                  borderRadius: '50%'
+                }}
+              >
                  <FiSettings/>
               </button>
             </TooltipComponent>
