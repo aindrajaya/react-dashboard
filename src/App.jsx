e968d9cd4792fa9b297a5646dc57cfabfc252ea7
Arista Indrajaya <arista.indrajaya@gmail.com>
2022-06-29 23:18:07 +0700

    update app.jsx
    add implementation for this react dashboard project styling with tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
-// import './App.css'
+import './App.css'
 
 const App = () => {
   return (
-    // <h1 className="underline text-3xl">Berubah</h1>
-    <div>Without tailwind</div>
+    <h1 className="underline text-3xl">Berubah</h1>
   )
 }
 
