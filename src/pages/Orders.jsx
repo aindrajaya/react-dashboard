3290068b961aafc20cdbe98b1d1a58a4dffef1c2
Arista Indrajaya <arista.indrajaya@gmail.com>
2022-07-24 04:50:00 +0700

    add grid table with empty data

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { GridComponent, ColumnsDirective, Resize, Sort, ContextMenu, Filter, Page, ExcelReport, PdfExport, Edit, Inject } from '@syncfusion/ej2-react-grids';
+import { GridComponent, ColumnsDirective, Resize, Sort, ContextMenu, Filter, Page, ExcelExport, PdfExport, Edit, Inject, ColumnDirective } from '@syncfusion/ej2-react-grids';
 
 import { ordersData, contextMenuItems, ordersGrid } from '../data/dummy';
 import { Header } from '../components';
@@ -8,6 +8,13 @@ const Orders = () => {
   return (
     <div className='m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl'>
       <Header category="Page" title="Orders"/>
+      <GridComponent>
+        <ColumnsDirective>
+          {ordersGrid.map((item, idx) => (
+            <ColumnDirective key={idx} {...item} />
+          ))}
+        </ColumnsDirective>
+      </GridComponent>
     </div>
   )
 }
