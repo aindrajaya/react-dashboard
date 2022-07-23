e9e376c0d8212477cb41c4a8ccb7bbe035048afd
Arista Indrajaya <arista.indrajaya@gmail.com>
2022-07-24 04:27:00 +0700

    orders page boilerplate

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import React from 'react';
+import { GridComponent, ColumnsDirective, Resize, Sort, ContextMenu, Filter, Page, ExcelReport, PdfExport, Edit, Inject } from '@syncfusion/ej2-react-grids';
+
+import { ordersData, contextMenuItems, ordersGrid } from '../data/dummy';
+import { Header } from '../components';
 
 const Orders = () => {
   return (
-    <div>Orders</div>
+    <div>
+      <Header category="Page" title="Orders"/>
+    </div>
   )
 }
 
