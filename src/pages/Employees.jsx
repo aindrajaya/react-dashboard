822570b279aefae1e11d7a873b8a368e0470ab09
Arista Indrajaya <arista.indrajaya@gmail.com>
2022-07-25 04:44:00 +0700

    table employees with data

--- src/pages/Employees.jsx
+++ src/pages/Employees.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { GridComponent, ColumnsDirective, Page, Search, Edit, Inject, ColumnDirective } from '@syncfusion/ej2-react-grids';
+import { GridComponent, ColumnsDirective, Page, Search, Edit, Inject, ColumnDirective, Sort } from '@syncfusion/ej2-react-grids';
 
 import { employeesData, employeesGrid } from '../data/dummy';
 import { Header } from '../components';
@@ -9,14 +9,17 @@ const Employees = () => {
     <div className='m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl'>
       <Header category="Page" title="Employees"/>
       <GridComponent
-        id='gridcomp'
+        dataSource={employeesData}
+        allowPaging
+        allowSorting
+        width="auto"
       >
         <ColumnsDirective>
           {employeesGrid.map((item, idx) => (
             <ColumnDirective key={idx} {...item}/>
           ))}
         </ColumnsDirective>
-        <Inject services={[Page, Search]}/>
+        <Inject services={[Page, Search, Sort]}/>
       </GridComponent>
     </div>
   )
