345c7edbb84a9087652b9e1c5fc65692e510f3e0
Arista Indrajaya <arista.indrajaya@gmail.com>
2022-07-26 04:12:00 +0700

    add kanban template

--- src/pages/Kanban.jsx
+++ src/pages/Kanban.jsx
@@ -7,7 +7,14 @@ import { Header } from '../components';
 const Kanban = () => {
   return (
     <div className='m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl'>
-      <Header category='App' title='Kanban'/>  
+      <Header category='App' title='Kanban'/>
+      <KanbanComponent
+        id='kanban'
+        keyField='Status'
+        dataSource={kanbanData}
+        cardSettings={{contentField: 'Summary', headerField:'Id'}}
+      >
+      </KanbanComponent>  
     </div>
   )
 }
